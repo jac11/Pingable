f2161ab603bac51ca80d8da74e34627cc7205768
jac11 <rootx1982@gmail.com>
2021-10-12 00:14:43 +0400

    Add files via upload

--- Pingable.py
+++ Pingable.py
@@ -1,8 +1,36 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 
+import argparse
+import sys
+class Run :
+        def __init__(self):
+            self.args_command()  
+            self.control()
+        def control(self):
+           if self.args.network  :
+              from Package.Pingclass import  Discover_Network
+              run = Discover_Network()
+              exit()
+#           elif self.args.pingclass and self.args.strat :
+#              from Package.Start_range import gi
+           else:
+             if self.args.Host :
+                 from Package.OneHost import  Host_One
+                 run = Host_One()
+                 exit()
+        def args_command(self):
+              parser = argparse.ArgumentParser( description="Usage: <OPtion> <arguments> ")
+              parser.add_argument( '-N',"--network"   ,metavar='' , action=None  ,help ="ping all Network ,IPaddress/subnet ")
+              parser.add_argument( '-H',"--Host"   ,metavar='' , action=None  ,help ="Ping One Host Only ")
+              parser.add_argument( '-O',"--output"   ,metavar='' , action=None  ,help ="out put file report ")
+             # Parser.add_argument( '-R', 
 
+              self.args = parser.parse_args()
+              if len(sys.argv)> 1 :
+                   pass
+              else:
+                   parser.print_help()
+                   exit()
 
-from Package_Ping.Pingclass import *
-
-run = Discover_Network()
-      
+if __name__=="__main__":
+   Run()
 
