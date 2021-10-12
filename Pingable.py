be168fcd2a29f1541581126ad082b06e19603e22
jac11 <rootx1982@gmail.com>
2021-10-12 16:02:05 +0400

    Add files via upload

--- Pingable.py
+++ Pingable.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python3 
+
 import argparse
 import sys
+
 class Run :
         def __init__(self):
             self.args_command()  
             self.control()
         def control(self):
-           if self.args.network  :
+           if self.args.network and not self.args.start and not self.args.end :
               from Package.Pingclass import  Discover_Network
               run = Discover_Network()
               exit()
-#           elif self.args.pingclass and self.args.strat :
-#              from Package.Start_range import gi
-           else:
-             if self.args.Host :
+           elif self.args.start and self.args.end :
+              from Package.HostRange import RangeOfHosts
+              run = RangeOfHosts()
+              exit()
+           elif self.args.Host :
                  from Package.OneHost import  Host_One
                  run = Host_One()
                  exit()
         def args_command(self):
               parser = argparse.ArgumentParser( description="Usage: <OPtion> <arguments> ")
               parser.add_argument( '-N',"--network"   ,metavar='' , action=None  ,help ="ping all Network ,IPaddress/subnet ")
-              parser.add_argument( '-H',"--Host"   ,metavar='' , action=None  ,help ="Ping One Host Only ")
-              parser.add_argument( '-O',"--output"   ,metavar='' , action=None  ,help ="out put file report ")
-             # Parser.add_argument( '-R', 
+              parser.add_argument( '-H',"--Host"      ,metavar='' , action=None  ,help ="Ping One Host Only ")
+              parser.add_argument( '-O',"--output"    ,metavar='' , action=None  ,help ="out put file report ")
+              parser.add_argument( '-S',"--start"     ,metavar='' , action=None  ,help ="ping range of ips start from")
+              parser.add_argument( '-E',"--end"       ,metavar='' , action=None  ,help ="en of the range of ips to ping ")
 
               self.args = parser.parse_args()
               if len(sys.argv)> 1 :
